df82a86cce8cd82cb30ca9871cde402a962fff3f
Jack Le Hamster <jacklehamster@gmail.com>
2024-01-27 13:12:10 -0800

    Auto-commit: 2024-01-27 13:12:10
    Files:\ncommit-all.sh

--- commit-all.sh
+++ commit-all.sh
@@ -6,10 +6,12 @@ git add .
 # Get the list of files about to be committed
 files=$(git status --porcelain | awk '{print $2}')
 
-# Commit with a message including file information
-commit_message="Auto-commit: $(date +"%Y-%m-%d %H:%M:%S") - Files:"
+# Commit with a multi-line message including file information
+commit_message="Auto-commit: $(date +"%Y-%m-%d %H:%M:%S")
+
+Files:"
 for file in $files; do
-  commit_message+=" $file"
+  commit_message+="\n$file"
 done
 
 git commit -m "$commit_message"
