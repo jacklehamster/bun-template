3893d1ad773c7d041d99ae92e873205f340f6ac3
Jack Le Hamster <jacklehamster@gmail.com>
2024-01-27 13:10:54 -0800

    Auto-commit: 2024-01-27 13:10:54 - Files: commit-all.sh

--- commit-all.sh
+++ commit-all.sh
@@ -3,8 +3,16 @@
 # Add all changes to the staging area
 git add .
 
-# Commit with a default message
-git commit -m "Auto-commit: $(date +"%Y-%m-%d %H:%M:%S")"
+# Get the list of files about to be committed
+files=$(git status --porcelain | awk '{print $2}')
+
+# Commit with a message including file information
+commit_message="Auto-commit: $(date +"%Y-%m-%d %H:%M:%S") - Files:"
+for file in $files; do
+  commit_message+=" $file"
+done
+
+git commit -m "$commit_message"
 
 # Print a message indicating the successful commit
 echo "Changes committed successfully."
