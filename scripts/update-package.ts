2c8e200e41faf46024d3503338727b7a1639ac9d
Jack Le Hamster <jacklehamster@gmail.com>
2024-01-27 17:50:28 -0800

    Auto-commit: 2024-01-27 17:50:28
    Files:\npackage.json\nscripts/update-package.ts

--- scripts/update-package.ts
+++ scripts/update-package.ts
@@ -34,13 +34,57 @@ function getAuthorName() {
   }
 }
 
+function getRepoOwner(repoUrl: string): string {
+  const match = repoUrl.match(/[:/]([^/]+)\/([^/]+)(\.git)?$/);
+  return match?.[1] ?? "";
+}
+
 function getRepoUrl() {
   try {
     // Get the HTTPS URL of the origin repository
     const repoUrl = execSync('git remote get-url origin').toString().trim();
     return repoUrl;
   } catch (error) {
-    return new Error('Error retrieving repository URL:', error.message);
+    throw new Error('Error retrieving repository URL:', error.message);
+  }
+}
+
+async function getRepoDetails(owner: string, repo: string) {
+  try {
+    const response = await fetch(`https://api.github.com/repos/${owner}/${repo}`);
+    const data: any = await response.json();
+
+    if (response.ok) {
+      return {
+        description: data.description || null,
+        owner: data.owner ? { id: data.owner.id } : null,
+        homepage: data.homepage || null,
+      };
+    } else {
+      console.error(`Failed to fetch repository details: ${data.message}`);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching repository details:', error.message);
+    return null;
+  }
+}
+
+async function getRepoDescription(owner: string, repo: string): Promise<string | null> {
+  console.log(owner, repo);
+  try {
+    const response = await fetch(`https://api.github.com/repos/${owner}/${repo}`);
+    const data: any = await response.json();
+
+    if (response.ok) {
+      return data.description || null;
+    } else {
+      console.error(`Failed to fetch repository details: ${data.message}`);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching repository details:', error.message);
+    return null;
   }
 }
 
@@ -48,6 +92,11 @@ async function updatePackage() {
   const file = Bun.file('package.json');
 
   const repoName = getRepoName();
+  const repoAuthor = getAuthorName();
+  const repoEmail = getAuthorEmail();
+  const repoUrl = getRepoUrl();
+  const repoDetails = await getRepoDetails(getRepoOwner(repoUrl), repoName);
+
   const pkg = JSON.parse(await file.text());
   if (pkg.name !== repoName) {
     pkg.name = repoName;
@@ -55,9 +104,12 @@ async function updatePackage() {
   }
   pkg.repository.url = getRepoUrl();
   pkg.author = {
-    name: getAuthorName(),
-    email: getAuthorEmail(),
+    name: repoAuthor,
+    email: repoEmail,
   };
+  pkg.description = repoDetails?.description ?? "<fill in description>";
+  pkg.homepage = repoDetails?.homepage ?? "<fill in homepage>";
+
   await Bun.write(file, JSON.stringify(pkg, null, "  ") + "\n");
   console.log(pkg);
 }
