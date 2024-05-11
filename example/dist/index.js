4d94eec3defb8a7cf7fbd1cf6a63e99b9a2da11a
Jack Le Hamster <jacklehamster@gmail.com>
2024-05-11 13:02:00 -0700

    Auto-commit: 2024-05-11 13:02:00
    Files:
    bun.lockb
    example/bun.lockb
    example/dist/index.js
    example/package.json
    package.json
    
    https://github.com/jacklehamster/autocommit

--- example/dist/index.js
+++ example/dist/index.js
@@ -1,9 +1,9 @@
 // /Users/vincent/bun-template/example/node_modules/bun-template/dist/index.js
-class e {
+class l {
   static hello() {
     return "Hello World!";
   }
 }
 export {
-  e as Hello
+  l as Hello
 };
